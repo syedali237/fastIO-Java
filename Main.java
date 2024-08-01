06e20490ce5fdbfa2761ebb9a098c7cad9fec144
syedali237 <syedaliulhasan19@gmail.com>
2024-08-02 02:17:25 +0530

    my template for Fast i/o java

---
+++ Main.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        // Test Cases
+        int t = in.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+        out.close();
+    }
+
+    static void solve() {
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
